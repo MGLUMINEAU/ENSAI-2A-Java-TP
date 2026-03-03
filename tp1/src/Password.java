c6d3839f8afafcf0f1483d81d0d28b573856f923
martingm <martin.glumineau@eleve.ensai.fr>
2026-03-03 11:14:34 +0000

    rawr

--- tp1/src/Password.java
+++ tp1/src/Password.java
@@ -65,11 +65,43 @@ public class Password {
      * @return true if the password is strong, false otherwise
      */
     public static boolean isStrongPassword(String password) {
-
-        // Code here
-
+    if (password.length() < 12) {
         return false;
+    } // le teste initiale 
+    
+    // Initialise évènement (drapeau) associé à chaque critère 
+    boolean hasUppercase = false;
+    boolean hasLowercase = false;
+    boolean hasDigit = false;
+    boolean noWhitespace = true;
+    
+    // Vérifie chaque caractère
+    for (char c : password.toCharArray()) {
+        // Si espace trouvé, le mot de passe n'est pas fort
+        if (Character.isWhitespace(c)) {
+            noWhitespace = false;
+        }
+        // n'est vrai que si on a pas d'espace 
+
+        // Vérifie majuscule (n'est vrai que si on a une majuscule)
+        if (Character.isUpperCase(c)) {
+            hasUppercase = true;
+        }
+        
+        // Vérifie minuscule (n'est vrai que si on a une minuscule)
+        if (Character.isLowerCase(c)) {
+            hasLowercase = true;
+        }
+        
+        // Vérifie chiffre (n'est vrai que si on a un chifre)
+        if (Character.isDigit(c)) {
+            hasDigit = true;
+        }
     }
+    
+    // Fort seulement si tous les critères sont remplis 
+    return hasUppercase && hasLowercase && hasDigit && noWhitespace;
+}   // on calcule l'intersection de tout ces boolen
 
     /**
      * Checks the strength of multiple passwords and stores the results in a
